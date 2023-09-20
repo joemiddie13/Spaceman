1de2db53ea5f654279483620c1a82499f99286d6
joemiddie13 <joemiddie13@gmail.com>
2023-09-20 15:40:45 -0700

    Function to return a string representing guesses.

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,6 @@
 import random
 
+# Open up the words.txt file, store in variable words_list, separate the words into a variable secret_word
 def load_word():
     f = open('words.txt', 'r') #opens the file 'words.txt' in read mode 'r' and stores the file object in variable 'f'
     words_list = f.readlines() #reads all the lines from the file f into a list, where each element of the list is a line from the file. This list is stored in the variable words_list.
@@ -9,70 +10,50 @@ def load_word():
     secret_word = random.choice(words_list) #picks a random word from the list and stores it in 'secret_word'
     return secret_word #returns the 'secret_word' effectively making it the output of 'load_word' function
 
+# Function checks if all the letters in secret_word are present in letters_guessed
 def is_word_guessed(secret_word, letters_guessed):
     for letter in secret_word:
         if letter not in letters_guessed:
             return False
     return True
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
-
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns: 
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
-
+ 
+# Function to generate and return a string that represents the letters correctly guessed so far, underscores letters incorrect
 def get_guessed_word(secret_word, letters_guessed):
+    word = ''
+    for letter in secret_word:
+        if letter in letters_guessed:
+            word += letter
+        else:
+            word += '_'
+        return word
     
-    
-    '''
-    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
-
-    Args: 
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns: 
-        string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
-    pass
+#TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
 
 
-def is_guess_in_word(guess, secret_word):
-    '''
-    A function to check if the guessed letter is in the secret word
 
-    Args:
-        guess (string): The letter the player guessed this round
-        secret_word (string): The secret word
+#def is_guess_in_word(guess, secret_word):
+    # '''
+    # A function to check if the guessed letter is in the secret word
 
-    Returns:
-        bool: True if the guess is in the secret_word, False otherwise
+    # Args:
+    #     guess (string): The letter the player guessed this round
+    #     secret_word (string): The secret word
 
-    '''
+    # Returns:
+    #     bool: True if the guess is in the secret_word, False otherwise
+
+    # '''
     #TODO: check if the letter guess is in the secret word
 
-    pass
 
+#def spaceman(secret_word):
+    # '''
+    # A function that controls the game of spaceman. Will start spaceman in the command line.
 
+    # Args:
+    #   secret_word (string): the secret word to guess.
 
-
-def spaceman(secret_word):
-    '''
-    A function that controls the game of spaceman. Will start spaceman in the command line.
-
-    Args:
-      secret_word (string): the secret word to guess.
-
-    '''
+    # '''
 
 
     #TODO: show the player information about the game according to the project spec
@@ -85,11 +66,6 @@ def spaceman(secret_word):
 
     #TODO: check if the game has been won or lost
 
-
-
-
-
-
 #These function calls that will start the game
-secret_word = load_word()
-spaceman(secret_word)
+# secret_word = load_word()
+# spaceman(secret_word)
